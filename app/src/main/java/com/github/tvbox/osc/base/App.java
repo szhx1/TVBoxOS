c840d0af5ae3256cefde040fb15ccb40eba74c88
szhx1 <162309171+szhx1@users.noreply.github.com>
2024-03-06 15:58:34 +0800

    Update App.java

--- app/src/main/java/com/github/tvbox/osc/base/App.java
+++ app/src/main/java/com/github/tvbox/osc/base/App.java
@@ -1,18 +1,25 @@
 package com.github.tvbox.osc.base;
 
+import android.app.Activity;
 import androidx.multidex.MultiDexApplication;
-
+import com.github.catvod.crawler.JsLoader;
+import com.github.tvbox.osc.bean.VodInfo;
 import com.github.tvbox.osc.callback.EmptyCallback;
 import com.github.tvbox.osc.callback.LoadingCallback;
 import com.github.tvbox.osc.data.AppDataManager;
 import com.github.tvbox.osc.server.ControlManager;
+import com.github.tvbox.osc.util.AppManager;
+import com.github.tvbox.osc.util.EpgUtil;
+import com.github.tvbox.osc.util.FileUtils;
 import com.github.tvbox.osc.util.HawkConfig;
+import com.github.tvbox.osc.util.LOG;
 import com.github.tvbox.osc.util.OkGoHelper;
 import com.github.tvbox.osc.util.PlayerHelper;
+
 import com.kingja.loadsir.core.LoadSir;
 import com.orhanobut.hawk.Hawk;
-
-import me.jessyan.autosize.AutoSize;
+import com.p2p.P2PClass;
+import com.whl.quickjs.android.QuickJSLoader;
 import me.jessyan.autosize.AutoSizeConfig;
 import me.jessyan.autosize.unit.Subunits;
 
@@ -24,13 +31,18 @@ import me.jessyan.autosize.unit.Subunits;
 public class App extends MultiDexApplication {
     private static App instance;
 
+    private static P2PClass p;
+    public static String burl;
+    private static String dashData;
+
     @Override
     public void onCreate() {
         super.onCreate();
         instance = this;
         initParams();
         // OKGo
-        OkGoHelper.init();
+        OkGoHelper.init(); //台标获取
+        EpgUtil.init();
         // 初始化Web服务器
         ControlManager.init(this);
         //初始化数据库
@@ -44,6 +56,8 @@ public class App extends MultiDexApplication {
                 .setSupportSP(false)
                 .setSupportSubunits(Subunits.MM);
         PlayerHelper.init();
+        QuickJSLoader.init();
+        FileUtils.cleanPlayerCache();
     }
 
     private void initParams() {
@@ -53,12 +67,47 @@ public class App extends MultiDexApplication {
         if (!Hawk.contains(HawkConfig.PLAY_TYPE)) {
             Hawk.put(HawkConfig.PLAY_TYPE, 1);
         }
-     if (!Hawk.contains(HawkConfig.IJK_CODEC)) {
-        Hawk.put(HawkConfig.IJK_CODEC, "硬解码");
-      }
     }
 
     public static App getInstance() {
         return instance;
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+        JsLoader.load();
+    }
+
+
+    private VodInfo vodInfo;
+    public void setVodInfo(VodInfo vodinfo){
+        this.vodInfo = vodinfo;
+    }
+    public VodInfo getVodInfo(){
+        return this.vodInfo;
+    }
+
+    public static P2PClass getp2p() {
+        try {
+            if (p == null) {
+                p = new P2PClass(instance.getExternalCacheDir().getAbsolutePath());
+            }
+            return p;
+        } catch (Exception e) {
+            LOG.e(e.toString());
+            return null;
+        }
+    }
+
+    public Activity getCurrentActivity() {
+        return AppManager.getInstance().currentActivity();
+    }
+
+    public void setDashData(String data) {
+        dashData = data;
+    }
+    public String getDashData() {
+        return dashData;
     }
 }
